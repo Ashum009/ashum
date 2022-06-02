af697c5e9c44bfcd9ee66bdcb2f5cc6c134fec42
Ashum009 <106086832+Ashum009@users.noreply.github.com>
2022-06-02 13:50:41 +0100

    new files

---
+++ script.js
@@ -0,0 +1,46 @@
+var fadeElements = document.getElementsByClassName('scrollFade');
+
+function scrollFade() {
+  var viewportBottom = window.scrollY + window.innerHeight;
+
+  for (var index = 0; index < fadeElements.length; index++) {
+    var element = fadeElements[index];
+    var rect = element.getBoundingClientRect();
+
+    var elementFourth = rect.height/4;
+    var fadeInPoint = window.innerHeight - elementFourth;
+    var fadeOutPoint = -(rect.height/2);
+
+    if (rect.top <= fadeInPoint) {
+      element.classList.add('scrollFade--visible');
+      element.classList.add('scrollFade--animate');
+      element.classList.remove('scrollFade--hidden');
+    } else {
+      element.classList.remove('scrollFade--visible');
+      element.classList.add('scrollFade--hidden');
+    }
+
+    if (rect.top <= fadeOutPoint) {
+      element.classList.remove('scrollFade--visible');
+      element.classList.add('scrollFade--hidden');
+    }
+  }
+}
+
+document.addEventListener('scroll', scrollFade);
+window.addEventListener('resize', scrollFade);
+document.addEventListener('DOMContentLoaded', function() {
+  scrollFade();
+});
+
+const ToTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () =>{
+  if(window.pageYOffset > 100){
+    ToTop.classList.add('active');
+  }else{
+    ToTop.classList.remove('active');
+  }
+
+
+});
